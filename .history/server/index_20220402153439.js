7c51381ddb203ed7d1d94e56ee54a6147fef5632
Mario Capitbrok <mario@capitbrok.com>
2022-04-02 15:56:03 -0600

    Part3. Database configuration into its own module

--- .history/server/index_20220402153439.js
+++ .history/server/index_20220402153439.js
@@ -1,9 +1,39 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
 const Note = require('./models/note')
 
 const app = express()
+
+if (process.argv.length < 3) {
+  console.log(
+    'Please provide the password as an argument: node mongo.js <password>'
+  )
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://mariocapitbrok:${password}@testcluster.kj2xo.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+const Note = mongoose.model('Note', noteSchema)
 
 let notes = [
   {
