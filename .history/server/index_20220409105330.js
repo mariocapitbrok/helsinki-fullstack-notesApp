721716ee2c6cea86e6d29241609c34141707e849
Mario Capitbrok <mario@capitbrok.com>
2022-04-09 10:56:49 -0500

    Part3. End

--- .history/server/index_20220409105330.js
+++ .history/server/index_20220409105330.js
@@ -5,6 +5,27 @@ const Note = require('./models/note')
 
 const app = express()
 
+let notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2022-01-10T17:30:31.098Z',
+    important: true,
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2022-01-10T18:39:34.091Z',
+    important: false,
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2022-01-10T19:20:14.298Z',
+    important: true,
+  },
+]
+
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
   console.log('Path:  ', request.path)
